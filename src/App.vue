0c3ad5c107f8f91fb77e4d213c394898a4b912b4
Noushedul1 <noushedakib@gmail.com>
2021-11-05 23:58:30 +0600

    try to clear all the vue concept clear

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,60 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <ComHome></ComHome>
+  <p>{{fname}}</p>
+  <p><a :href=google target="_blank">vue</a></p>
+  <div v-html="code"></div>
+  <p v-text="msg"></p>
+  <img :src=photo alt="">
+  <ul>
+    <li v-for="range in rollrange" :key="range">
+      <a href="">{{range}}</a>
+    </li>
+  </ul>
+  <button @click="hello">click</button>
+  <div v-if="ifElsecondition">
+    if is true 
+  </div>
+  <div v-else>
+    if is false
+  </div>
+  <div v-if="elseifCondition === 'D' ">
+    A
+  </div>
+  <div v-else-if="elseifCondition === 'B' ">
+    B
+  </div>
+  <div v-else-if="elseifCondition === 'C' ">
+    C
+  </div>
+  <div v-else>
+    no Alphabet
+  </div>
 </template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+<script> 
+  import ComHome from './components/Home.vue';
+  export default{
+    components: {ComHome},
+    data(){
+      return{
+        fname: 'noudshed',
+        sallary: 2000,
+        bonus: 1000,
+        google: 'https://v3.vuejs.org/guide/installation.html#release-notes',
+        code: '<h1>hello noushed</h1>',
+        msg: 'southern university of bangladesh',
+        photo: 'https://source.unsplash.com/random/201x200?computer',
+        rollrange: [1,2,4,5,6],
+        ifElsecondition: true,
+        elseifCondition: 'A'
+      }
+    },
+    methods:{
+      hello(){
+        console.log(typeof this.bonus);
+      }
+    }
   }
-}
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped>
+  
 </style>
---
+++ src/components/Home.vue
@@ -0,0 +1,111 @@
+<template>
+    <div v-show="country === 'Bangladesh'">
+        Bangladesh
+    </div>
+    <div v-show="country === 'uk'">
+        another country
+    </div>
+    <ul>
+        <li v-for="item in items" :key="item">{{item.message}}</li>
+    </ul>
+    <ul>
+        <li href="#" v-for="(winterFood, index) in winterFoods" v-bind:key="winterFood">
+            <a href="">{{country}}-{{index}}-{{winterFood}}</a>
+        </li>
+    </ul>
+    <div v-for="(value , name , index) in book" v-bind="books" :key="value">
+        {{index}} : {{name}} : {{value}}
+    </div>
+    <div v-for="n in 10" v-bind:key="n">
+        {{n}}
+    </div>
+    <div>
+       result is : {{resultNum}}
+    </div>
+    <button @click="addNumber">addnumber</button>
+
+    <div v-once>
+        old city name: {{city}}
+    </div>
+    <div>
+        updated city name: {{city}}
+        <!-- {{update()}} -->
+    </div>
+    <button v-on:click="update()">update city</button>
+    <ul>
+        <li v-for="rolls in roll" v-bind:key="rolls">
+            <a href="">{{rolls}}</a>
+        </li>
+    </ul>
+    <button v-on:click="rollArray">roll array</button>
+    <button v-on:click="counter">counter</button>
+    <p>counter : {{count}}</p>
+    <button v-on:click="decrement">decrement</button>
+    <button v-on:click="myalert">alert</button>
+</template>
+<script>
+    export default{
+        data(){
+            return{
+                country: 'uk',
+                items: [
+                    {message: 'foo'},
+                    {message: 'fii'}
+                ],
+                winterFoods: ['coffi','shak','mula'],
+                count: 0,
+                book: {
+                    title: 'the cook book',
+                    author: 'urk',
+                    date: '06-12-2021'
+                },
+                num1: 20,
+                num2: 30,
+                resultNum: '',
+                city: 'chittagong',
+                roll: [9,2,3,4,5,6,7,8,1,10,11,11,13,12]
+            }
+        },
+        methods: {
+            addNumber: function(){
+                this.resultNum =  this.num1 + this.num2;
+            },
+            update: function(){
+                setTimeout(()=>{
+                    this.city = 'chattogram';
+                },3000);
+            },
+            rollArray: function(){
+                // this.roll.push(10);
+                // shift delete from start one by one
+                // this.roll.shift(10);
+                // pop delete from the dend one by one
+                // this.roll.pop();
+                // this.roll.splice(5);
+                // this.roll.reverse();
+                // this.roll.sort((a,b)=>a-b);
+                // return this.roll.filter((a)=>{
+                //     return a % 2 === 0;
+                // });
+                // [...new Set(this.roll)]
+            },
+            counter: function(){
+                this.count += 1;
+            },
+            decrement: function(){
+                this.count -= 1;
+            },
+            myalert: function(){
+                alert('hello vue js');
+            }
+        }
+    }
+</script>
+<style scoped>
+    ul{
+        list-style-type: none;
+    }
+    a{
+        text-decoration: none;
+    }
+</style>
